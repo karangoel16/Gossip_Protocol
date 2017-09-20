0e3f7ee3433a4f35bbdd804211a2dd265ae880ca
karangoel16 <karangoel16@yahoo.com>
2017-09-20 05:02:01 -0400

    final version

--- lib/client.ex
+++ lib/client.ex
@@ -6,7 +6,6 @@ defmodule Project2.Client do
         #Project2.Exdistutils.start_distributed(Integer.to_string(args)|>String.to_atom)
         #IO.inspect Node.self()
         #IO.inspect  :"project2@10.228.7.203"
-        loop()
     end
 
     def init(:ok) do
@@ -15,37 +14,42 @@ defmodule Project2.Client do
     def cast_call(check,msg,name) do
         GenServer.cast({Integer.to_string(name)|>String.to_atom,Node.self()},{check,msg,name})
     end
-    def handle_cast({check , msg , name},state) do
+    def handle_call({check , msg , name},_from,state) do
       case check do
-          :msg->map=elem(state,1)
-                case Map.get(map,msg,0)>10 do
-                    #Need to call GenServer and Stop it
-                    true->
-                        IO.puts("Request Full")
-                    {:noreply,state}
-                    false->var=:rand.uniform(tuple_size(elem(state,0)))
-                           map=Map.put(map,msg,Map.get(map,msg,0)+1)
-                           GenServer.cast({ String.to_atom(Integer.to_string(elem(elem(state,0),var-1))),Node.self() },{:msg,msg,elem(elem(state,0),var-1)})
-                           cast_call(check,msg,name)
-                           {:noreply,{elem(state,0),map}}
-                    end
+          
           :complete->
                 var=Enum.to_list(1..String.to_integer(msg))|>List.to_tuple
                 var=Tuple.delete_at(var,name-1)#delete yourself from list
                 temp={var,elem(state,1)}
-                IO.inspect temp
-                {:noreply,temp }
+                {:reply,"",temp }
           :line->#we need to add previous and the new state here
                 var=Tuple.append(elem(state,0),msg)
-                {:noreply,{var,elem(state,1)}}
+                {:reply,"",{var,elem(state,1)}}
           :link->#we made this to check our network
-                {:noreply,state}
+                {:reply,state,state}
       end
     end
-
+    def handle_cast({:msg , msg , name},state) do
+        map=elem(state,1)
+            case Map.get(map,msg,0)>10 do
+        #Need to call GenServer and Stop it
+            true->
+                #IO.puts("hello")
+                var=:rand.uniform(tuple_size(elem(state,0)))
+                GenServer.call({:Server,Node.self()},{:add_val,name},:infinity)
+                {:noreply,state}
+            false->
+                var=:rand.uniform(tuple_size(elem(state,0)))
+                map=Map.put(map,msg,Map.get(map,msg,0)+1)
+                #IO.inspect map
+                GenServer.cast({elem(elem(state,0),var-1)|>Integer.to_string|>String.to_atom,Node.self() },{:msg,msg,elem(elem(state,0),var-1)})
+               #IO.puts map
+               spawn(fn->cast_call(:msg,msg,name)end)
+               {:noreply,{elem(state,0),map}}
+            end
+        end
 
     def loop do
         Process.sleep(1_000_000)
-        loop()
     end
 end
--- lib/project2.ex
+++ lib/project2.ex
@@ -1,31 +1,49 @@
 defmodule Project2 do
   use GenServer
-  def start_link do
-    GenServer.start_link(__MODULE__,:ok,name: :Server)
+  def start_link(args) do
+    GenServer.start_link(__MODULE__,args,name: :Server)
   end
-  def init(:ok) do
-    {:ok,%{}}
+  def init(args) do
+    {:ok,{args|>String.to_integer,%{}}}
   end
 
   def main(args\\[]) do
     if List.to_tuple(args)|>tuple_size<2 do
-      Process.stop()
+      IO.puts ("here")
+      :init.stop()
     end
     number_of_node=elem(List.to_tuple(args),0)
     Project2.Exdistutils.start_distributed(:project2)
-    start_link() #this is where the server genserver starts
+    start_link(number_of_node) #this is where the server genserver starts
     IO.puts "... build topology"
-    temp=:os.system_time(:millisecond)
-    Enum.map(1..String.to_integer(number_of_node),fn(x)->spawn(fn->Project2.Client.start_link(Integer.to_string(x)|>String.to_atom) end)end)
+    GenServer.cast({:Server,Node.self()},{:add_time,:os.system_time(:millisecond)})
+    IO.inspect Enum.map(1..String.to_integer(number_of_node),fn(x)->spawn(fn->Project2.Client.start_link(Integer.to_string(x)|>String.to_atom) end)end)
+    
     case elem(List.to_tuple(args),1)|>String.to_atom do
-      :full->Enum.map(1..String.to_integer(number_of_node),fn(x)->GenServer.cast({Integer.to_string(x)|>String.to_atom,Node.self()},{:complete,number_of_node,x})end)
-      :line->Enum.map(2..String.to_integer(number_of_node),fn(x)->GenServer.cast({Integer.to_string(x)|>String.to_atom,Node.self()},{:line,x-1,x})end)
+      :full->Enum.map(1..String.to_integer(number_of_node),fn(x)->GenServer.call({Integer.to_string(x)|>String.to_atom,Node.self()},{:complete,number_of_node,x},:infinity)end)
+      :line->Enum.map(2..String.to_integer(number_of_node),fn(x)->GenServer.call({Integer.to_string(x)|>String.to_atom,Node.self()},{:line,x-1,x},:infinity)end)
              #this is for backward adding of the nodes
-             Enum.map(1..(String.to_integer(number_of_node)-1),fn(x)->GenServer.cast({Integer.to_string(x)|>String.to_atom,Node.self()},{:line,x+1,x})end)
+             Enum.map(1..(String.to_integer(number_of_node)-1),fn(x)->GenServer.call({Integer.to_string(x)|>String.to_atom,Node.self()},{:line,x+1,x},:infinity)end)
              #this is for forward adding of the terminal in the state of the GenServer
       end
-    IO.puts(:os.system_time(:millisecond)-temp)
-    #loop()
+    GenServer.cast({:"3",Node.self()},{:msg,"hello",3})
+    loop()
+  end
+
+  def handle_cast({:add_time,time},state) do
+    {:noreply,Tuple.append(state,time)}
+  end
+  
+  def handle_call({:add_val,name},_from,state) do
+    map=Map.put(elem(state,1),name,1)
+    state=Tuple.delete_at(state,1)
+    state=Tuple.insert_at(state,1,map)
+    if length(Map.to_list(elem(state,1)))==elem(state,0) do
+      IO.puts(:os.system_time(:millisecond)-elem(state,2))
+      Enum.map(1..elem(state,0),fn(x)->GenServer.stop({Integer.to_string(x)|>String.to_atom,Node.self()})end)
+      Process.exit(self(),:normal)
+    end
+    {:reply,"",state}
   end
 
   def spawnner(number_of_node,temp\\0) do
@@ -39,6 +57,7 @@ defmodule Project2 do
   end
 
   def loop do
-    loop
+    Process.sleep(1_000_000)
+    #loop
   end
 end
